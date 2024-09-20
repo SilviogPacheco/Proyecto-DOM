7c4db7a1d6981f1670e39ea92ad796cf40544acf
SilviogPacheco <megaboy93mile@gmail.com>
2024-09-19 21:58:09 -0300

    Agrego funcionalidad de los metodos de Repository + función de creación de cards

--- scripts/index.js
+++ scripts/index.js
@@ -10,13 +10,50 @@ class Activity {
 class Repository {
   constructor() {
     this.activities = [];
+    this.id = 1;
   }
 
-  addActivity(activity) {}
+  generateId() {
+    return this.id++;
+  }
 
-  getActivities() {}
+  addActivity(title, description, imgUrl) {
+    const id = this.generateId();
+    const activity = new Activity(id, title, description, imgUrl);
+    this.activities.push(activity);
+  }
 
-  deleteActivity(id) {}
+  getActivities() {
+    return this.activities;
+  }
+
+  deleteActivity(id) {
+    this.activities = this.activities.filter((activity) => activity.id !== id);
+  }
 }
 
 const repository = new Repository();
+
+function createActivityCard(activity) {
+  const { id, title, description, imgUrl } = activity;
+
+  const cardDiv = document.createElement("div");
+  const titleElement = document.createElement("h3");
+  const descriptionElement = document.createElement("p");
+  const imgElement = document.createElement("img");
+
+  titleElement.innerHTML = title;
+  descriptionElement.innerHTML = description;
+  imgElement.src = imgUrl;
+
+  cardDiv.classList.add("activity-card");
+  titleElement.classList.add("activity-title");
+  descriptionElement.classList.add("activity-description");
+  imgElement.classList.add("activity-img");
+
+  cardDiv.appendChild(imgElement);
+  cardDiv.appendChild(titleElement);
+  cardDiv.appendChild(descriptionElement);
+
+  return cardDiv;
+}
