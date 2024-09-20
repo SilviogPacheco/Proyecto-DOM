9aef1dfde6c830b6c7a7eb39b8c02b98b60b2e3d
SilviogPacheco <megaboy93mile@gmail.com>
2024-09-19 21:15:03 -0300

    creación de las clases Activity y Repository

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+class Activity {
+  constructor(id, title, description, imgUrl) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.imgUrl = imgUrl;
+  }
+}
+
+class Repository {
+  constructor() {
+    this.activities = [];
+  }
+
+  addActivity(activity) {}
+
+  getActivities() {}
+
+  deleteActivity(id) {}
+}
+
+const repository = new Repository();
